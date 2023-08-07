2fa0887a890532f0d4e81807eadcdffb03eca29b
Dmitri Serebryakov <serebryakov169@gmail.com>
2023-08-07 23:02:42 +0300

    add burger and adaptiv

--- js/script.js
+++ js/script.js
@@ -31,3 +31,24 @@ let swiper = new Swiper(".swiper", {
 swiper.on("click", function (e) {
   console.log("Click on slide", e.clickedIndex);
 });
+
+
+//Смена картинок по времени
+
+let index = 0,
+          container = document.querySelector(".background-block");
+
+        function autochange() {
+          let image = ['../../img/проекты/01.jpg', '../../img/проекты/03.jpg', '../../img/проекты/04.jpg',  '../../img/проекты/05.jpg', '../../img/проекты/06.jpg', '../../img/проекты/07.jpg', '../../img/проекты/08.jpg'];
+          container.style.backgroundImage = 'url(' + image[index++] + ')';
+          if (index > 6) {
+            index = 0;
+          }
+        }
+        window.setInterval(autochange, 3000);
+
+
+//Бургер
+
+
+
