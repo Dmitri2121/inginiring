0317aecccced6fcbdc72e024ae5a9a6c029a3098
Dmitri Serebryakov <serebryakov169@gmail.com>
2023-08-09 18:55:48 +0300

    add fix bag

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ let index = 0,
           container = document.querySelector(".background-block");
 
         function autochange() {
-          let image = ['../img/проекты/01.jpg', '../img/проекты/03.jpg', '../img/проекты/04.jpg',  '../img/проекты/05.jpg', '../img/проекты/06.jpg', '../img/проекты/07.jpg', '../img/проекты/08.jpg'];
+          let image = ['img/проекты/01.jpg', 'img/проекты/03.jpg', 'img/проекты/04.jpg',  'img/проекты/05.jpg', 'img/проекты/06.jpg', 'img/проекты/07.jpg', 'img/проекты/08.jpg'];
           container.style.backgroundImage = 'url(' + image[index++] + ')';
           if (index > 6) {
             index = 0;
